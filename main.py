2150eb9af928c4b1a25d86f7c648e90c5e603738
akiyomov <114338623+akiyomov@users.noreply.github.com>
2022-12-02 17:58:31 +0900

    more cleaner

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
-import pandas as pd
-import requests
+
 import datetime
 
-url = "https://kun.uz/uz/news/2022/11/30/parda-ortidagi-xususiylashtirish-yirik-aktivlar-qanday-qilib-ofshor-kompaniyalarga-otib-ketmoqda"
 def getText(url):
     req = Request(
         url = url, 
@@ -13,9 +11,9 @@ def getText(url):
     webpage = urlopen(req).read()
     soup = BeautifulSoup(webpage, features="html.parser")
     time = datetime.datetime.now()
-    textElem = soup.find_all('p',attrs={"dir":"auto"}) 
+    textElem = soup.find('div',attrs={"class":"single-content"}) 
     text = ""
-    for element in textElem:
+    for element in textElem.find_all('p'):
         text += element.text
     return [url,time,text]
 
